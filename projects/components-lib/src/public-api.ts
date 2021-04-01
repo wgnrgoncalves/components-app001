2496185d5e5365b6c62bd26c34dcb999470c0fe9
Wagner Gonçalves <wgnrgoncalves@gmail.com>
2021-03-31 22:22:15 -0300

    Removi o service

--- projects/components-lib/src/public-api.ts
+++ projects/components-lib/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of components-lib
  */
 
-export * from './lib/components-lib.service';
+
 export * from './lib/components-lib.component';
 export * from './lib/components-lib.module';
