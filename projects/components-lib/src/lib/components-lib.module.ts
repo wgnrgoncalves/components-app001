f5168c9f91b26efa533ae5a7c41286d8fb16e2b8
Wagner Gonçalves <wgnrgoncalves@gmail.com>
2021-03-31 21:17:44 -0300

    first commit

---
+++ projects/components-lib/src/lib/components-lib.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ComponentsLibComponent } from './components-lib.component';
+
+
+
+@NgModule({
+  declarations: [ComponentsLibComponent],
+  imports: [
+  ],
+  exports: [ComponentsLibComponent]
+})
+export class ComponentsLibModule { }
